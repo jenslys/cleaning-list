f14f04820d5cea308e13823dd04fb612713768dc
Jens <69081683+jenslys@users.noreply.github.com>
2023-08-09 15:27:21 +0200

    use seed generation insead of json file + add vercl conf

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 const app = express();
 const port = 3000;
+const seedrandom = require("seedrandom");
 
 // Function to get the current week number
 function getCurrentWeekNumber() {
@@ -46,18 +47,17 @@ const restrictions = {
   Aksel: ["Toppetasjen", "Toppetasjen Bad"],
 };
 
-// Function to generate the cleaning list
-function generateCleaningList() {
-  // Shuffle the rooms to randomize the assignment
-  const shuffledRooms = rooms.slice().sort(() => 0.5 - Math.random());
+// Function to generate the cleaning list based on a given seed
+function generateCleaningList(seed) {
+  const rng = seedrandom(seed); // Use the seedrandom function
+  const shuffledRooms = rooms.slice().sort(() => rng() - 0.5);
 
-  // Assign rooms to people based on restrictions
   const cleaningList = {};
   for (const person of people) {
     const personRestrictions = restrictions[person] || [];
     const availableRooms = shuffledRooms.filter((room) => {
       const isRestricted = personRestrictions.includes(room);
-      console.log(`${person} - ${room} - Restricted: ${isRestricted}`);
+      //console.log(`${person} - ${room} - Restricted: ${isRestricted}`);
       return !isRestricted;
     });
 
@@ -69,57 +69,17 @@ function generateCleaningList() {
     }
   }
 
-  // Save the cleaning list to a file (cleaning-list.json)
-  fs.writeFile(
-    "cleaning-list.json",
-    JSON.stringify(cleaningList, null, 2),
-    (err) => {
-      if (err) {
-        console.error("Error saving cleaning list:", err);
-      } else {
-        console.log("Cleaning list has been saved to cleaning-list.json");
-      }
-    }
-  );
-
   return cleaningList;
-}
-
-// Function to ping itself to keep the website alive on Render (https://render.com/docs/free#free-web-services)
-async function pingCleaningList() {
-  try {
-    const response = await fetch("https://cleaning-list.onrender.com/");
-    const data = await response.text();
-    console.log("Ping successful. Response:", data);
-  } catch (error) {
-    console.error("Error pinging cleaning list:", error);
-  }
 }
 
 // Route to display the cleaning list as an HTML list
 app.get("/", (req, res) => {
-  fs.readFile("cleaning-list.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading cleaning list:", err);
-      // If there was an error reading the file, generate a new cleaning list
-      const cleaningList = generateCleaningList();
-      const weekNumber = getCurrentWeekNumber();
-      res.render("index", { cleaningList, weekNumber });
-    } else {
-      try {
-        // Attempt to parse the JSON data from the file
-        const cleaningList = JSON.parse(data);
-        const weekNumber = getCurrentWeekNumber();
-        res.render("index", { cleaningList, weekNumber });
-      } catch (parseError) {
-        // If there was an error parsing the JSON data, generate a new cleaning list
-        console.error("Error parsing cleaning list:", parseError);
-        const cleaningList = generateCleaningList();
-        const weekNumber = getCurrentWeekNumber();
-        res.render("index", { cleaningList, weekNumber });
-      }
-    }
-  });
+  const weekNumber = getCurrentWeekNumber();
+  const seed = `Week${weekNumber}`; // Use the week number as the seed
+  const cleaningList = generateCleaningList(seed);
+
+  // Render the page with the cleaning list
+  res.render("index", { cleaningList, weekNumber });
 });
 
 // Set the view engine to EJS
@@ -128,26 +88,5 @@ app.use(express.static("public"));
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port:${port}`);
-  generateCleaningList(); // Generate the cleaning list when the server starts
-
-  // Schedule the list generation to run every Monday at 12:00 AM
-  cron.schedule(
-    "0 0 * * 1",
-    () => {
-      console.log("Generating the cleaning list...");
-      generateCleaningList();
-    },
-    { timezone: "Europe/Oslo" }
-  );
-
-  // Schedule the ping to run every 10 minutes
-  cron.schedule(
-    "*/10 * * * *",
-    () => {
-      console.log("Pinging Website");
-      pingCleaningList();
-    },
-    { timezone: "Europe/Oslo" }
-  );
+  console.log(`Server is running on port: ${port}`);
 });
